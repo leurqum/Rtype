86b580a8557c98ad8c11ffe82ace30afc3f9b636
marche_m <marche_m@epitech.net>
2013-01-07 16:45:46 +0100

    Porta socket

--- server/USocket.cpp
+++ server/USocket.cpp
@@ -5,7 +5,7 @@
 // Login   <marche_m@epitech.net>
 // 
 // Started on  Sat Dec 29 11:22:15 2012 marche_m (Maxime Marchès)
-// Last update Sun Dec 30 14:03:22 2012 marche_m (Maxime Marchès)
+// Last update Mon Jan  7 16:37:20 2013 marche_m (Maxime Marchès)
 //
 
 #include "USocket.h"
@@ -32,43 +32,15 @@ bool	USocket::connectToServer(std::string const & host, std::string const & port
       std::cerr << "error: gethostbyname no such host" << this->_host << std::endl;
       return 0;
     }
-  bzero((char *) &this->hints, sizeof(this->_hints));
+  bzero((char *) &this->_hints, sizeof(this->_hints));
   this->_hints.sin_family = AF_INET;
   bcopy((char *)this->_server->h_addr, (char *)&this->_hints.sin_addr.s_addr, this->_server->h_length);
-  this->_hints.sin_port = htons(atoi(this->portNo));
-  if (connect(this->sockFd,(struct sockaddr *) &this->servAddr,sizeof(this->servAddr)) < 0)
-    this->error("ERROR connecting");
-
-  /*
-    WSADATA			wsd;
-    DWORD			dwFlags = 0;
-    int				rc;
-
-    if ((rc = WSAStartup(MAKEWORD(2, 2), &wsd)))
+  this->_hints.sin_port = htons(atoi(this->_port.c_str()));
+  if (connect(this->_connectSocket,(struct sockaddr *) &this->_hints,sizeof(this->_hints)) < 0)
     {
-    std::cerr << "Unable to load Winsock: " << rc << std::endl;
-    return false;
+      std::cerr << "ERROR connecting" << std::endl;
+      return false;
     }
-
-    //Init of struct addrinfo
-    ZeroMemory((PVOID) &this->_hints, sizeof((this->_hints)));
-    this->_hints.ai_family = AF_INET;
-    this->_hints.ai_socktype = SOCK_STREAM;
-    this->_hints.ai_protocol = IPPROTO_TCP;
-
-
-    if ((rc = getaddrinfo(host.c_str(), port.c_str(), &(this->_hints), (&this->_results))) != 0 )
-    {
-    std::cerr << "getaddrinfo failed with error: " << rc << std::endl;
-    return false;
-    }
-    if ((this->_connectSocket = WSASocket(this->_results->ai_family, this->_results->ai_socktype, this->_results->ai_protocol, NULL, 0, 0)) == INVALID_SOCKET)
-    {
-    std::cerr << "Error at socket(): " << WSAGetLastError() << std::endl;
-    return false;
-    }
-    WSAConnect(this->_connectSocket, this->_results->ai_addr, this->_results->ai_addrlen, 0, 0, 0, 0);
-  */
   return true;
 }
 
@@ -82,44 +54,30 @@ bool	USocket::connectFromAcceptedFd(void * fd)
   return true;
 }
 
-int	USocket::recv(s_protocol * package)
+int		USocket::recv()
 {
-}
-
-
-int	USocket::recv(std::string & buffer, int blocksize)
-{
-}
-
-int	USocket::sendv(void * const & data, int const & size)
-{
-}
-
-int	USocket::sendv(s_protocol * const & package)
-{
-  int * header = new int[2];
-  void * data;
-  header[0] = package->cmdId;
-  header[1] = package->size;
-  data = package->data;
-  send(this->_connectSocket, (char*)header, 2 * sizeof(int), NULL);
-  send(this->_connectSocket, (char*)data, header[1], NULL);
-  delete header;
+  char toto[4242];
+  memset(toto, 0, 4242);
+  int ret = read(this->_connectSocket, toto, 4242);
+  if (ret <= 0)
+    return ret;
+  std::cout << toto << std::endl;
   return 1;
 }
 
-int	USocket::sendv(std::string const & data)
+int		USocket::sendv(std::string const & data)
 {
+  write(this->_connectSocket, data.c_str(), data.size());
 }
 
 USocket::~USocket()
 {
-  closesocket(this->_connectSocket);
+  close(this->_connectSocket);
 
   //Connect from accepted fd
   if (this->_close == true)
     {
       //      WSACleanup();
-      freeaddrinfo(this->_results);
+      //      freeaddrinfo(this->_results);
     }
 }
