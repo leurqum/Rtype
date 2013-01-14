55cd7e32a7180eb82d1a34c49513b7047025b4d9
carlsGI <carls.gipson@epitech.eu>
2013-01-14 09:38:28 -0800

    La c est des commit qui marche normalement

--- server/src/include/IAUnit.hpp
+++ server/src/include/IAUnit.hpp
@@ -4,19 +4,37 @@
 #include "Unit.hpp"
 #include "Game.hpp"
 
-
 class IAUnit : public Unit
 {
-  Game *_game;
-  Protocol::type_enemie _type;
+
 public:
-  IAUnit(std::pair<float, float> speed, Protocol::type_enemie type, int id, ICollisionDefinition *rec, int health, int strength, bool isDestroyable, Game *game)
-	  : Unit(speed, id, rec, health, strength, isDestroyable){ _game = game; _type = type;}
+  IAUnit(std::pair<float, float> speed, Protocol::type_enemie type, int id, ICollisionDefinition *rec
+		, int health, int strength, bool isDestroyable, Game *game, Protocol::patern_enemie patern)
+			  : Unit(speed, id, rec, health, strength, isDestroyable)
+	{ 
+		_game = game;
+		_type = type; 
+		_patern = patern; 
+		_toShoot = false;
+
+		this->_direction.left = 1;
+		this->_direction.top = 0;
+		this->_direction.down = 0;
+		this->_direction.right = 0;
+	};
   ~IAUnit(void);
-  void takeDecision();
+  void takeDecision(double);
   void executeDecision();
+  void chooseDirection();
   void update(double time);
   Protocol::type_enemie getType()const;
+  Protocol::patern_enemie getPatern() const;
+protected:
+  Game *_game;
+  Protocol::type_enemie _type;
+  Protocol::patern_enemie _patern;
+  bool _toShoot;
+  Protocol::move _direction;
 };
 
 #endif
--- server/src/source/IAUnit.cpp
+++ server/src/source/IAUnit.cpp
@@ -1,23 +1,62 @@
 #include "../include/IAUnit.hpp"
 
+
 IAUnit::~IAUnit(void)
 {
 }
+
+
 
 Protocol::type_enemie IAUnit::getType()const
 {
   return (_type);
 }
 
-void IAUnit::update(double time)
+Protocol::patern_enemie IAUnit::getPatern()const
 {
+	return (_patern);
 }
 
-void IAUnit::takeDecision()
+void IAUnit::update(double time)
 {
-  //utiliser move et fire_ia qui se trouve dans game
+		takeDecision(time);
+		executeDecision();
+}
+
+void IAUnit::chooseDirection()
+{
+	//if (this->_patern == 1)
+	//{
+
+	//}
+}
+void IAUnit::takeDecision(double time)
+{
+	int		cadence = 3;
+
+	if (time > 45)
+		cadence = 2;
+
+	if (this->_patern == 0 && (int)time % 4 == 0)
+		this->_toShoot = true;
+	
+	else
+		{
+			if ((int)time % cadence == 0)
+				this->_toShoot = true;
+			else
+			{
+				this->_toShoot = false;
+				chooseDirection();
+			}
+		}
 }
 
 void IAUnit::executeDecision()
 {
+	if (this->_toShoot == true)
+		this->_game->fire(this->getId());
+	else
+		this->_game->move(this->getId(),&(this->_direction));
+
 }
