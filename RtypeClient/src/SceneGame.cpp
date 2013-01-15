830767352c5ae0ec61ef20764cc4a865c1a9f1e7
Thierry Berger <tom.kahl@hotmail.fr>
2013-01-15 23:10:06 +0100

    setupdate from network a bit

--- RtypeClient/src/SceneGame.cpp
+++ RtypeClient/src/SceneGame.cpp
@@ -24,53 +24,61 @@ IScene* SceneGame::update(float elapsedTime)
   //std::cout << "scenegame update" << std::endl;
 
   // TODO: update the scene depending on network data received.
-  Protocol::drawable d;
-  d.id = 0;
-  d.type = Protocol::SHIP;
-  static int x = 490;
-  static int incr = 5;
+  // Protocol::drawable d;
+  // d.id = 0;
+  // d.type = Protocol::SHIP;
+  // static int x = 490;
+  // static int incr = 5;
 
-  d.xPosition = x;
-   x += incr;
-   if (x > 500)
-     incr = -5;
-   if (x <= 400)
-     incr = 5;
-  d.yPosition = 123;
+  // d.xPosition = x;
+  //  x += incr;
+  //  if (x > 500)
+  //    incr = -5;
+  //  if (x <= 400)
+  //    incr = 5;
+  // d.yPosition = 123;
 
 
-  if (ship[0] == nullptr)
-    {
-      ship[0] = new DrawerShip(0);
-    }
   // NOTE: we assume d.type is the right type for old, if it's a wrong type, then it's messed up earlier. (but this class doesn't care, it's Drawable who will handle (or not) the matter.)
-  ship[0]->setUpdate(d);
+  
 
-#define pos(x) ((x) > 0 ? (x) : -(x))
-  drawer_2bars.setBar1(pos(x - 500));
+  Protocol::drawable d = network->GetPieceWorld();
 
-  d.id = 0;
-  d.type = Protocol::SHIP;
-  static int _x = 480;
-  static int _y = 223;
-  static int _incr = 2;
+  if (d.type == Protocol::type_drawable::SHIP)
+    {
+      if (ship[0] == nullptr)
+	{
+	  ship[0] = new DrawerShip(0);
+	}
+      ship[0]->setUpdate(d);
+    }
 
-  drawer_2bars.setBar2(pos(_x - 400));
-  // drawer_2bars.setBar2(100);
+// #define pos(x) ((x) > 0 ? (x) : -(x))
+//   drawer_2bars.setBar1(pos(x - 500));
 
-  d.xPosition = _x;
-  _x += _incr;
-  _y += _incr;
-   if (_x > 500)
-     _incr = -3;
-   if (_x <= 400)
-     _incr = 3;
+//   d.id = 0;
+//   d.type = Protocol::SHIP;
+//   static int _x = 480;
+//   static int _y = 223;
+//   static int _incr = 2;
 
-  d.yPosition = _y;
-  enemy.setUpdate(d);
+//   drawer_2bars.setBar2(pos(_x - 400));
+//   // drawer_2bars.setBar2(100);
 
-  enemy.update(elapsedTime);
-  ship[0]->update(elapsedTime);
+//   d.xPosition = _x;
+//   _x += _incr;
+//   _y += _incr;
+//    if (_x > 500)
+//      _incr = -3;
+//    if (_x <= 400)
+//      _incr = 3;
+
+//   d.yPosition = _y;
+//   enemy.setUpdate(d);
+
+//   enemy.update(elapsedTime);
+  if (ship[0])
+    ship[0]->update(elapsedTime);
   drawer_2bars.update(elapsedTime);
 
 
