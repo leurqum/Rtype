ce0e44febd0839bc8458871997bd5fe9042385ec
Thierry Berger <tom.kahl@hotmail.fr>
2013-01-16 11:09:44 +0100

    testserver

---
+++ RtypeClient/test/serverTest.cpp
@@ -0,0 +1,21 @@
+#include <SFML/Network.hpp>
+
+
+int main(int ac, char** av)
+{
+  sf::UdpSocket s;
+  sf::IpAdress ip = sf::IpAdress::getLocalAdress();
+
+  s.bind(4243);
+  
+  while (1);
+  {
+    Protocol::drawable d;
+    d.type = Protocol::type_drawable::SHIP;
+    d.id = 0;
+    d.xPosition = 20;
+    d.yPosition = 20;
+    s.send(&d, sizeof(d), ip, 4243);
+  }
+  return 0;
+}
